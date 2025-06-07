27a53d92f4ef1540ad061f83b42d2e72f7804f3a
Tryk47 <jesper.tryk@hotmail.com>
2025-06-07 21:26:54 +0200

    2. first GUI created.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 
 
@@ -11,6 +12,20 @@ import pygame
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
 
 
 
